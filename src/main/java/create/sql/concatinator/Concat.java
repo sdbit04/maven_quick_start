3d2a2e84dc48bb4eb61b9565594eeb942d3ef991
Swapan kumar Das <sdbit04@gmail.com>
2018-03-20 09:51:08 +0530

    Dir structure and application file has been added

---
+++ src/main/java/create/sql/concatinator/Concat.java
@@ -0,0 +1,23 @@
+
+package create.sql.concatinator;
+public class Concat {
+public static void main (String args [] )
+{
+
+	String sql = "Insert Into Users (name, email, pass, address)";
+
+//	sql += " values ('" + user + "'";
+	sql = sql + " values ('" + "Swapan" + "'";
+//  In the above code multiple string objects are created, as String is 
+//	immutable.
+//	Whereas only the final object are in use. and object creation take more time thereby 
+//	performance will be bad.
+	System.out.println(sql);
+	
+//	StringBuilder is the solution for the same.
+	StringBuilder sqlSB = new StringBuilder("Insert Into Users (name, email, pass, address)");
+	sqlSB.append(" values ('" + "Swapan" + "')");
+	System.out.println(sqlSB);
+	
+}
+}
